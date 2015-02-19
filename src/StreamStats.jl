dd522a5a8dbb7b3eef15c57cd1d67c152ea859d5
Sean Garborg <sean.garborg@gmail.com>
2015-02-19 07:02:03 -0700

    Add REQUIRE file, don't export rand

--- src/StreamStats.jl
+++ src/StreamStats.jl
@@ -1,7 +1,7 @@
 module StreamStats
     import StatsBase, Distributions
 
-    export update!, state, nobs, rand, ci
+    export update!, state, nobs, ci
 
     include("streamstat.jl")
     include("mean.jl")
