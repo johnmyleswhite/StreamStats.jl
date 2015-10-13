c61dcc2f6b98fce37b6ba4e077942e600cc701a6
Peter Colberg <peter@colberg.org>
2015-10-13 00:29:58 -0400

    Use immutable stats objects

--- src/moments.jl
+++ src/moments.jl
@@ -1,46 +1,28 @@
-type Moments <: ContinuousMultivariateStreamStat
+immutable Moments <: ContinuousMultivariateStreamStat
     m1::Float64
     m2::Float64
     m3::Float64
     m4::Float64
-    n::Int
+    n::Int64
 end
 
-Moments() = Moments(0.0, 0.0, 0.0, 0.0, 0)
+Base.zero(::Type{Moments}) = Moments(0.0, 0.0, 0.0, 0.0, 0)
 
-function update!(stat::Moments, x::Real)
-    stat.n += 1
-
-    n = stat.n
+function Base.(:+)(stat::Moments, x::Real)
+    n = stat.n + 1
 
     δ = x - stat.m1
-    δ_n = δ / stat.n
+    δ_n = δ / n
     δ_n_sq = δ_n * δ_n
     term1 = δ * δ_n * (n - 1)
 
-    stat.m1 += δ_n
-    stat.m4 += term1 * δ_n_sq * (n * n - 3 * n + 3) +
-               6 * δ_n_sq * stat.m2 - 4 * δ_n * stat.m3
-    stat.m3 += term1 * δ_n * (n - 2) - 3 * δ_n * stat.m2
-    stat.m2 += term1
+    m1 = stat.m1 + δ_n
+    m2 = stat.m2 + term1
+    m3 = stat.m3 + term1 * δ_n * (n - 2) - 3 * δ_n * stat.m2
+    m4 = stat.m4 + term1 * δ_n_sq * (n * n - 3 * n + 3) +
+         6 * δ_n_sq * stat.m2 - 4 * δ_n * stat.m3
 
-    return
-end
-
-function Base.mean(stat::Moments)
-    return stat.m1
-end
-
-function Base.var(stat::Moments)
-    return stat.m2 / (stat.n - 1)
-end
-
-function StatsBase.skewness(stat::Moments)
-    return sqrt(stat.n) * stat.m3 / stat.m2^1.5
-end
-
-function StatsBase.kurtosis(stat::Moments)
-    return stat.n * stat.m4 / (stat.m2 * stat.m2) - 3.0
+    return Moments(m1, m2, m3, m4, n)
 end
 
 nobs(stat::Moments) = stat.n
@@ -51,41 +33,30 @@ end
 
 Base.copy(stat::Moments) = Moments(stat.m1, stat.m2, stat.m3, stat.m4, stat.n)
 
-function Base.merge(a::Moments, b::Moments)
-    merged = Moments()
-
-    merged.n = a.n + b.n
+function Base.(:+)(a::Moments, b::Moments)
+    n = a.n + b.n
 
     δ = b.m1 - a.m1
     δ2 = δ * δ
     δ3 = δ * δ2
     δ4 = δ2 * δ2
 
-    merged.m1 = (a.n * a.m1 + b.n * b.m1) / merged.n
+    m1 = (a.n * a.m1 + b.n * b.m1) / n
 
-    merged.m2 = a.m2 + b.m2 + δ2 * a.n * b.n / merged.n
+    m2 = a.m2 + b.m2 + δ2 * a.n * b.n / n
 
-    merged.m3 = a.m3 + b.m3 +
-                  δ3 * a.n * b.n * (a.n - b.n) / (merged.n * merged.n)
-    merged.m3 += 3.0 * δ * (a.n * b.m2 - b.n * a.m2) / merged.n
+    m3 = a.m3 + b.m3 +
+                  δ3 * a.n * b.n * (a.n - b.n) / (n * n)
+    m3 += 3.0 * δ * (a.n * b.m2 - b.n * a.m2) / n
 
-    merged.m4 = a.m4 + b.m4 + δ4 * a.n * b.n *
+    m4 = a.m4 + b.m4 + δ4 * a.n * b.n *
                   (a.n * a.n - a.n * b.n + b.n * b.n) /
-                  (merged.n * merged.n * merged.n)
-    merged.m4 += 6.0 * δ2 * (a.n * a.n * b.m2 + b.n * b.n * a.m2) /
-                   (merged.n * merged.n) +
-                   4.0 * δ * (a.n * b.m3 - b.n * a.m3) / merged.n
+                  (n * n * n)
+    m4 += 6.0 * δ2 * (a.n * a.n * b.m2 + b.n * b.n * a.m2) /
+                   (n * n) +
+                   4.0 * δ * (a.n * b.m3 - b.n * a.m3) / n
 
-    return merged
-end
-
-function Base.empty!(stat::Moments)
-    stat.n = 0
-    stat.m1 = 0.0
-    stat.m2 = 0.0
-    stat.m3 = 0.0
-    stat.m4 = 0.0
-    return
+    return Moments(m1, m2, m3, m4, n)
 end
 
 function Base.show(io::IO, stat::Moments)
